e713c4e8a67a7e6331bcbeeea908dbc2b260df66
my0866805011 <my0866805011@gmail.com>
2023-02-08 21:57:31 +0700

    ep09

--- lib/states/authen.dart
+++ lib/states/authen.dart
@@ -11,29 +11,99 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  bool mStatusRedEye = true;
   @override
   Widget build(BuildContext context) {
     double mSize = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
-        child: ListView(
-          children: [
-            dispImgage(mSize),
-            appName(),
-            userName(mSize),
-          ],
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+          child: ListView(
+            children: [
+              dispImgage(mSize),
+              appName(),
+              userName(mSize),
+              userPassword(mSize),
+            ],
+          ),
         ),
       ),
     );
   }
 
   Row userName(double mSize) {
-    return Row(mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(width: mSize*0.6,
-                child: TextFormField()),
-            ],
-          );
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+            margin: EdgeInsets.only(top: 16),
+            width: mSize * 0.6,
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelStyle: Config().h3Style(),
+                labelText: 'User :',
+                prefixIcon: Icon(
+                  Icons.account_circle_outlined,
+                  color: Config.dart,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Config.dart),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Config.light),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            )),
+      ],
+    );
+  }
+
+  Row userPassword(double mSize) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+            margin: EdgeInsets.only(top: 16),
+            width: mSize * 0.6,
+            child: TextFormField(
+              obscureText: mStatusRedEye,
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        mStatusRedEye = !mStatusRedEye;
+                      });
+                    },
+                    icon: mStatusRedEye
+                        ? Icon(
+                            Icons.remove_red_eye,
+                            color: Config.dart,
+                          )
+                        : Icon(
+                            Icons.remove_red_eye_outlined,
+                            color: Config.dart,
+                          )),
+                labelStyle: Config().h3Style(),
+                labelText: 'Password :',
+                prefixIcon: Icon(
+                  Icons.lock_outline,
+                  color: Config.dart,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Config.dart),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Config.light),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            )),
+      ],
+    );
   }
 
   Row appName() {
@@ -53,6 +123,7 @@ class _AuthenState extends State<Authen> {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Container(
+          margin: EdgeInsets.only(top: 16),
           width: mSize * 0.35,
           child: ShowImage(cPath: Config.image2),
         ),
