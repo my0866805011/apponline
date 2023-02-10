6bc8750860aa3f230ca9a46eca56f82e72be89ab
my0866805011 <my0866805011@gmail.com>
2023-02-10 19:38:26 +0700

    ep12

--- lib/states/create_account.dart
+++ lib/states/create_account.dart
@@ -12,15 +12,56 @@ class CreateAccount extends StatefulWidget {
 class _CreateAccountState extends State<CreateAccount> {
   @override
   Widget build(BuildContext context) {
+     double mSize = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text('Create New Account'),
         backgroundColor: Config.primary,
       ),
-      body: ShowTitle(
-        cTitle: 'ข้อมูลทั่วไป',
-        cTextStyle: Config().h2Style(),
-        ),
+      body: ListView(
+        children: [
+          titleMsg('ข้อมูลทั่วไป'),
+          userName(mSize),
+          titleMsg('ประเภท User'),
+        ],
+      ),
     );
   }
+
+  ShowTitle titleMsg(String ctitle) {
+    return ShowTitle(
+      cTitle: ctitle,
+      cTextStyle: Config().h3Style(),
+      );
+  }
 }
+ Row userName(double mSize) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+            margin: const EdgeInsets.only(top: 16),
+            width: mSize * 0.6,
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelStyle: Config().h3Style(),
+                labelText: 'User :',
+                prefixIcon: Icon(
+                  Icons.account_circle_outlined,
+                  color: Config.dart,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Config.dart),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Config.light),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            )),
+      ],
+    );
+  }
+
+
